e62db82e1a039de0b494456add4c323e514b57e6
Julian D Silva <neon.d.silvam@gmail.com>
2026-02-13 11:35:22 -0800

    Reapply "ADDED: Proxy stuff"
    This reverts commit ff1c2903fbf705d7ea8f474ac7b23f29ed1184bb.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,5 +14,11 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
-  },
+    },
+    server: {
+        proxy:{
+                "/api": "http://localhost:3000"
+        }
+    }
+
 })
